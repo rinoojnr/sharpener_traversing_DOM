375451b2eb7cf175126989967e81d4a67f616075
rinoojnr <rinoojnr@gmail.com>
2023-10-06 13:56:46 +0530

    Added CreateElement  styles

--- index.js
+++ index.js
@@ -1,45 +1,75 @@
-var itemList = document.querySelector('#items');
+// var itemList = document.querySelector('#items');
 
-//parentNode
-// itemList.parentNode.style.backgroundColor ='red';
-// console.log(itemList.parentNode.parentNode.parentNode)
-
-
-//parentElements
-// itemList.parentElement.style.backgroundColor='green';//same as parentNode
-
-// //childNodes
-// console.log(itemList.childNodes)//nodeList
+// //parentNode
+// // itemList.parentNode.style.backgroundColor ='red';
+// // console.log(itemList.parentNode.parentNode.parentNode)
 
 
-// //children
-// console.log(itemList.children)
-// console.log(itemList.children[0].style.backgroundColor='red') //html collection
+// //parentElements
+// // itemList.parentElement.style.backgroundColor='green';//same as parentNode
 
-// //firstChild
-// console.log(itemList.firstChild,"===============firstchild==========")
+// // //childNodes
+// // console.log(itemList.childNodes)//nodeList
 
-// //firstElementChild
-// console.log(itemList.firstElementChild,"=================firstelementChild==============");
-// itemList.firstElementChild.textContent='helooo';
 
-//lastChild
-// console.log(itemList.lastChild)
+// // //children
+// // console.log(itemList.children)
+// // console.log(itemList.children[0].style.backgroundColor='red') //html collection
 
-// //lastElementChild
+// // //firstChild
+// // console.log(itemList.firstChild,"===============firstchild==========")
 
-// console.log(itemList.lastElementChild,"===================lastElementchild====================")
-// itemList.lastElementChild.style.backgroundColor='red'
+// // //firstElementChild
+// // console.log(itemList.firstElementChild,"=================firstelementChild==============");
+// // itemList.firstElementChild.textContent='helooo';
 
-//nextSibling
-console.log(itemList.nextSibling)
+// //lastChild
+// // console.log(itemList.lastChild)
 
-//nextElementSibling
-console.log(itemList.nextElementSibling,"==========================nextElementSibling=================")
+// // //lastElementChild
 
-//previousSibling
-console.log(itemList.previousSibling,"===================previousSibling==================================")
+// // console.log(itemList.lastElementChild,"===================lastElementchild====================")
+// // itemList.lastElementChild.style.backgroundColor='red'
 
-//previousElementSibling
-console.log(itemList.previousElementSibling,"======================previousElementSibling================")
-itemList.previousElementSibling.style.color='red';
+// //nextSibling
+// console.log(itemList.nextSibling)
+
+// //nextElementSibling
+// console.log(itemList.nextElementSibling,"==========================nextElementSibling=================")
+
+// //previousSibling
+// console.log(itemList.previousSibling,"===================previousSibling==================================")
+
+// //previousElementSibling
+// console.log(itemList.previousElementSibling,"======================previousElementSibling================")
+// itemList.previousElementSibling.style.color='red';
+
+
+//createElement
+
+//create div
+var newDiv = document.createElement('div')
+console.log(newDiv)
+
+//Add class
+newDiv.className='main-class';
+
+//Add id
+newDiv.id ='main-class-id'
+
+//Add attribute
+newDiv.setAttribute('title','hello div');
+
+//Create newText node
+var newDivText =document.createTextNode("Hello world")
+
+//Added text node into a div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 =document.querySelector('header h1');
+newDiv.style.backgroundColor='red'
+container.insertBefore(newDiv,h1)
+
+
+
