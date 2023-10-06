96f19d3b0888052d5073ac32bcb571f47ea24f5f
rinoojnr <rinoojnr@gmail.com>
2023-10-06 12:02:19 +0530

    Added childNodes,childre,firstChild,firstElementChild styles

--- index.js
+++ index.js
@@ -6,4 +6,19 @@ var itemList = document.querySelector('#items');
 
 
 //parentElements
-itemList.parentElement.style.backgroundColor='green';//same as parentNode
+// itemList.parentElement.style.backgroundColor='green';//same as parentNode
+
+//childNodes
+console.log(itemList.childNodes)//nodeList
+
+
+//children
+console.log(itemList.children)
+console.log(itemList.children[0].style.backgroundColor='red') //html collection
+
+//firstChild
+console.log(itemList.firstChild,"===============firstchild==========")
+
+//firstElementChild
+console.log(itemList.firstElementChild,"=================firstelementChild==============");
+itemList.firstElementChild.textContent='helooo';
